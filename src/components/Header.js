deaf29dd528aea6312af96a9c21048ee4ec3e516
Eddie Shin <eshinhw@gmail.com>
2023-03-03 23:33:03 -0500

    updated small details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "../css/Header.css";
 import headerImg from "../assets/bingo.png";
+import wallet from "../assets/wallet.png";
 
 function Header() {
   return (
-    <div className="header">
-      <div className="logo">
-        <img src={headerImg} />
-        <p>Dottery</p>
-        <img src={headerImg} />
+    <>
+      <div className="header">
+        <div className="logo">
+          <img src={headerImg} />
+          <div className="logo__text">
+            <p>Dottery</p>
+            <p style={{fontSize: "30px"}}>Play with Your Crypto!</p>
+          </div>
+
+          <img src={headerImg} />
+        </div>
       </div>
-    </div>
+      {/* <img className="wallet" src={wallet} /> */}
+    </>
   );
 }
 
